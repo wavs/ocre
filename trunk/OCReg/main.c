15c990ee3f9a0e69099ed89aa0855eac28d45afa
atestu <atestu@0426016d-d63e-0410-b645-ad462c66d616>
2008-01-26 16:49:31 +0000

    mise a la norme et ajout de headers

--- trunk/OCReg/main.c
+++ trunk/OCReg/main.c
@@ -1,3 +1,15 @@
+/*
+	OCRe - The ultimate OCR - HUGE Software
+	OCRe is a project developed by 2nd year EPITA students
+	- website: http://huge.ocre.free.fr/
+	- svn repository: http://code.google.com/p/ocre
+
+	About this folder:	OCReg
+		OCRed is the GUI part of OCRe. 
+	About this file:	OCReg/main.c
+		This is the main file.
+*/
+
 #include "main.h"
 
 int     main (int argc, char *argv[])
