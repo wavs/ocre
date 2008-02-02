8ebc95af62547c87282d032085241c192e9f96a7
atestu <atestu@0426016d-d63e-0410-b645-ad462c66d616>
2008-02-02 14:27:06 +0000

    ajout du dossier gui

---
+++ gui/hello.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+int main(int argv, char *argv[])
+{
+    int age = 0;
+    (void)printf("Quel age avez vous ? ");
+    (void)scanf("%d", &age);
+ 
+    if (age < 18)
+    {
+        (void)puts("Vous etes mineur !");
+    }
+    else
+    {
+        (void)puts("Vous etes majeur !");
+    }
+ 
+    return EXIT_SUCCESS;
+}
