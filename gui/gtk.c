7436dd1b1c219ac0490165434a3f341ba485f803
atestu <atestu@0426016d-d63e-0410-b645-ad462c66d616>
2008-02-02 14:39:26 +0000

    ajout de gtk.c

---
+++ gui/gtk.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "/opt/gtk/include/gtk-2.0/gtk/gtk.h"
+
+int main (int argc, char *argv[])
+{
+    GladeXML        *gxml;
+    GtkWidget       *window;
+
+    gtk_init (&argc, &argv);
+
+    gxml = glade_xml_new ("tutorial.glade", NULL, NULL);
+    window = glade_xml_get_widget (gxml, "window");
+    glade_xml_signal_connect (gxml, "on_window_destroy",
+                              G_CALLBACK (gtk_main_quit));
+
+    g_object_unref (G_OBJECT (gxml));
+        
+    gtk_widget_show (window);                
+    gtk_main ();
+
+    return 0;
+}
