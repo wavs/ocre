6f07ce329122f9ca1f5d29984e1ee73932249bdd
guilbep <guilbep@0426016d-d63e-0410-b645-ad462c66d616>
2008-02-09 04:50:54 +0000

    add of file MODIFY

--- OCRed/main.ml
+++ OCRed/main.ml
@@ -3,7 +3,7 @@
 	OCRe is a project developed by 2nd year EPITA students
 	- website: http://huge.ocre.free.fr/
 	- svn repository: http://code.google.com/p/ocre
-	
+
 	About this folder:	OCRed
 		OCRed is the preprocessing part of OCRe.
 	About this file:	OCRed/main.ml
