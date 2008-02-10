5f876a7408c87bde8c80ab2351ec12a91cb5a7a6
dimitri.georgoulis <dimitri.georgoulis@0426016d-d63e-0410-b645-ad462c66d616>
2008-02-10 11:12:14 +0000

    seuil par dimi

--- trunk/OCRed/argument.ml
+++ trunk/OCRed/argument.ml
@@ -12,27 +12,33 @@
 
 (** {2 definition of functions for arguments} *)
 let display = ref false
+let seuil = ref false
 
 let set_path_image s =
-Path.create_image s;
-Surface.set_image !Path.image
+  Path.create_image s;
+  Surface.set_image !Path.image
 
 let set_path_output s =
-Path.output := s
+  Path.output := s
+
+let set_seuil i =
+  Seuil.seuil := i;
+  seuil := true
 
 
 let set_angle a =
-Rotation.create_angle a
+  Rotation.create_angle a
 
 let speclist = [
   ("-o", Arg.String (fun s -> set_path_output s), "output path of your ouput image");
   ("-i", Arg.String (fun s -> set_path_image s), "input path of your input image");
   ("-a", Arg.Int (fun i -> set_angle i), "integer Angle in degree");
   ("-d", Arg.Set display, " allows to see your image being transformed");
+  ("-s", Arg.Int (fun i -> set_seuil i), "integer  Treshold : Color to Black&White");
   ("--output", Arg.String (fun s -> set_path_output s), "output path of your ouput image");
   ("--image", Arg.String (fun s -> set_path_image s), "input path of your input image");
   ("--angle", Arg.Int (fun i -> set_angle i), "Angle in degree");
-  ("--display", Arg.Set display, "allows to see your image being transformed")
+  ("--display", Arg.Set display, " Useless option")
 ]
 
-let usage = "Usage: -image [path] -angle [integer]"
+let usage = "Usage: -image [path] -angle [float]"
--- trunk/OCRed/digit.mli
+++ trunk/OCRed/digit.mli
@@ -6,6 +6,22 @@
 	
 	About this folder:	OCRed
 		OCRed is the preprocessing part of OCRe. 
-	About this file:	OCRed/rotation.mli
-		This is the rotation.mli file.
+	About this file:	OCRed/digit.mli
+		This is the digit.mli file.
 *)
+                 
+val division : 'a list -> 'a list * 'a list
+val fusion : 'a list -> 'a list -> 'a list
+val tri_fusion : 'a list -> 'a list
+val tabtolist : 'a array -> 'a list
+val mediane : 'a list -> 'a
+val filtremed :
+  (int32, 'a, 'b) Bigarray.Array2.t ->
+  (int32, Bigarray.int32_elt, Bigarray.c_layout) Bigarray.Array2.t                                             
+val lectureliste : int list -> unit
+val lecturebigarray : (int32, 'a, 'b) Bigarray.Array2.t 
+-> unit                                                
+val main2 : unit -> unit
+val main3 : unit -> unit
+val main4 : unit -> unit
+val main : unit -> unit
