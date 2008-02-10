bf4ef7e932a7bc43a56642ea88c3ee0a7224d673
atestu <atestu@0426016d-d63e-0410-b645-ad462c66d616>
2008-02-10 16:38:26 +0000

    vire ocaml makefile

--- camlp4/repeat_syn.ml
+++
@@ -1,9 +0,0 @@
-(*pp camlp4o pa_extend.cmo q_MLast.cmo *)
-
-open Pcaml;;
-
-EXTEND
-  expr: LEVEL "top"
-    [[ "repeat"; e1 = expr; "until"; e2 = expr; "done" ->
-      <:expr< do { $e1$; while not $e2$ do { $e1$; } } >> ]];
-END
