f48fe128d9f184d03d135c8d345210cb17c1cfeb
guilbep <guilbep@0426016d-d63e-0410-b645-ad462c66d616>
2008-02-28 17:08:33 +0000

    suppression des espaces dans le network2.ml

--- network/network2.ml
+++ network/network2.ml
@@ -1,6 +1,6 @@
 (***                                                     2008 © ***)
-(******************************************************************) 
-(* OCRe                                                           *) 
+(******************************************************************)
+(* OCRe                                                           *)
           (******)    (******)   (*****)  (****)
           (******)    (******)   (*****)  (****)
           (******)    (******)   (*****)  (****)
@@ -10,13 +10,13 @@
           (******)    (******)   (*************)
           (******)    (******)   (*************)
 
- 
+
                  (**************)     (***********)  (***)
                  (**************)     (***********)  (***)
                  (*****)              (****)         (***)
                  (*****) (********)   (********)     (***)
                  (*****) (********)   (********)     (***)
-                 (*****)    (***)     (****)        
+                 (*****)    (***)     (****)
                  (**************)     (***********)  (***)
                  (**************)     (***********)  (***)
 (*                                                                *)
@@ -24,26 +24,26 @@
 (***                                         by the HUGE Team © ***)
 
 
-  
+
 (*Neuron*)
 (*****************************begin********************************)
 class neuron =
-object 
+object
   val mutable output = 0.; (*valeur de sortie du neurone*)
   method getoutput() = output; (*retourner output*)
-  method setoutput output0 = output <- output0; (*modifier output*) 
+  method setoutput output0 = output <- output0; (*modifier output*)
 
   val mutable sum = 0.; (*somme pondérée prise en paramètre par la fonction d'activation*)
   method getsum() = sum; (*retourner netsum*)
-  method setsum sum0 = sum <- sum0; (*modifier netsum*) 
+  method setsum sum0 = sum <- sum0; (*modifier netsum*)
 
   val mutable delta = 0.; (*permet de calculer la nouvelle valeur du poids de chaque connection après rétro-propagation de l'erreur*)
   method getdelta() = delta; (*retourner delta*)
-  method setdelta delta0 = delta <- delta0; (*modifier delta*)      
+  method setdelta delta0 = delta <- delta0; (*modifier delta*)
 
   val mutable beta = 0.5; (*paramètre de la fonction d'activation (prendre 0,5, 1 ou 5 par exemple)*)
   method getbeta = beta; (*retourner beta*)
-  method setbeta beta0 = beta <- beta0; (*modifier beta*)                   
+  method setbeta beta0 = beta <- beta0; (*modifier beta*)
 
   val mutable weights = Array.make 1 0.5; (*vecteur de poids des connections avec les neurones précédents si il y en a*)
   method getweights() = weights; (*retourner weights*)
@@ -70,8 +70,8 @@ object
     for j = 0 to (Array.length weights) - 1 do
       print_string "* ";
       print_float (weights.(j));
-      print_newline()       
-    done         
+      print_newline()
+    done
 end
 (******************************end*********************************)
 
@@ -81,8 +81,8 @@ end
 class layer =
 object
   val mutable neurons = (*vecteur de neurones de la couche*)
-    Array.make 1 (new neuron); 
-  method getneurons() = neurons; (*retourner neurons*)        
+    Array.make 1 (new neuron);
+  method getneurons() = neurons; (*retourner neurons*)
   method setneurons neurons0 = neurons <- neurons0; (*modifier neurons*)
 
   method print() = (*affiche les infos des neurones d'une couche*)
@@ -90,7 +90,7 @@ object
       print_newline();
       (neurons.(i))#print();
       print_newline()
-    done 
+    done
 end
 (******************************end*********************************)
 
@@ -116,8 +116,8 @@ object (self)
       for j = 0 to (Array.length (layers.(i)#getneurons())) - 1 do (*on parcourt chaque neurone de chaque couche*)
         (layers.(i)#getneurons()).(j)#setsum 0.; (*on initialise l'activation à zéro*)
         for k = 0 to (Array.length (layers.(i - 1)#getneurons())) - 1 do (*on parcourt chaque neurone de la couche précédente*)
-          (layers.(i)#getneurons()).(j)#setsum ((layers.(i)#getneurons()).(j)#getsum() +. 
-                                                ((layers.(i - 1)#getneurons()).(k)#getoutput() +. bias) *. 
+          (layers.(i)#getneurons()).(j)#setsum ((layers.(i)#getneurons()).(j)#getsum() +.
+                                                ((layers.(i - 1)#getneurons()).(k)#getoutput() +. bias) *.
                                                 (((layers.(i)#getneurons()).(j)#getweights()).(k)))
         done; (*l'activation de chaque neurone de chaque couche est calculée et mise à jour au sein des neurones*)
       done;
@@ -134,12 +134,12 @@ object (self)
   method delta() = (*calcule le nouveau delta qui servira à calculer le nouveau poids de chaque connexion*)
     for i = 1 to (Array.length layers) - 1 do (*on parcourt la liste des couches*)
       for j = 0 to (Array.length (layers.(i)#getneurons())) - 1 do (*on traite différemment la couche de sortie des autres*)
-        if i = (Array.length layers) - 1 then 
-          (layers.(i)#getneurons()).(j)#setdelta (((*f'*)((layers.(i)#getneurons()).(j)#getsum())) *. 
+        if i = (Array.length layers) - 1 then
+          (layers.(i)#getneurons()).(j)#setdelta (((*f'*)((layers.(i)#getneurons()).(j)#getsum())) *.
                                                   ((*Tj*) -. (layers.(i)#getneurons()).(j)#getoutput()) )
         else
-          (layers.(i)#getneurons()).(j)#setdelta ((*f'*)((layers.(i)#getneurons()).(j)#getsum())) *. 
-                                                  (layers.(i)#getneurons()).(j + 1)#getdelta() *. 
+          (layers.(i)#getneurons()).(j)#setdelta ((*f'*)((layers.(i)#getneurons()).(j)#getsum())) *.
+                                                  (layers.(i)#getneurons()).(j + 1)#getdelta() *.
                                                   ((layers.(i)#getneurons()).(j + 1)#getweights()).(j))
  done;
  done
@@ -156,7 +156,7 @@ end
 
 let main() = (*Boucle principale*)
   for i = 1 to 42 do
-    print_string 
+    print_string
 ("
 
 
@@ -169,13 +169,13 @@ let main() = (*Boucle principale*)
 (******)    (******)   (*************)
 (******)    (******)   (*************)
 
- 
+
        (**************)     (***********)  (***)
        (**************)     (***********)  (***)
        (*****)              (****)         (***)
        (*****) (********)   (********)     (***)
        (*****) (********)   (********)     (***)
-       (*****)    (***)     (****)       
+       (*****)    (***)     (****)
        (**************)     (***********)  (***)
        (**************)     (***********)  (***)
 
