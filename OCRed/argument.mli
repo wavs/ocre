01844464cf86a5db665436b17e9595cdd75154c4
guilbep <guilbep@0426016d-d63e-0410-b645-ad462c66d616>
2008-03-22 23:57:17 +0000

    ca marche

--- OCRed/argument.mli
+++ OCRed/argument.mli
@@ -9,11 +9,19 @@
 	About this file:	OCRed/argument.mli
 		This is the argument.mli file.
 *)
-val display : bool ref
-val seuil : bool ref
-val rotate : bool ref
-val set_path_image : string -> unit
-val set_path_output : string -> unit
-val set_angle : int -> unit
-val speclist : (string * Arg.spec * string) list
-val usage : string
+val display             : bool ref
+val seuil               : bool ref
+val rotate              : bool ref
+val resize              : bool ref
+val percent             : bool ref
+val percent_res         : int ref
+val resize_of_x         : int ref
+val resize_of_y         : int ref
+val set_path_image      : string -> unit
+val set_path_output     : string -> unit
+val set_percent_resize  : int -> unit
+val set_angle           : int -> unit
+val set_width           : int -> unit
+val set_height          : int -> unit
+val speclist            : (string * Arg.spec * string) list
+val usage               : string
--- OCRed/interpolation.mli
+++ OCRed/interpolation.mli
@@ -9,3 +9,8 @@
 	About this file:	OCRed/interpolation.mli
 		This is the interpolation.mli file.
 *)
+val roundf              : float -> int
+val inverse_resize      : int -> int -> float -> float -> int * int
+val is_in_rect          : int -> int -> int -> int -> bool
+val resize              : Sdlvideo.surface -> int -> int -> Sdlvideo.surface
+val resize_percent      : Sdlvideo.surface -> int -> Sdlvideo.surface
