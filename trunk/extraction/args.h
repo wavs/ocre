2260d86d715b561f53ce989c586fc415c9e4fec7
wavox.fr <wavox.fr@0426016d-d63e-0410-b645-ad462c66d616>
2008-03-23 22:09:22 +0000

    Correction args.c/h

--- trunk/extraction/args.h
+++ trunk/extraction/args.h
@@ -1,21 +1,23 @@
-/*
-** OCRe - The ultimate OCR - HUGE Software
-** OCRe is a project developed by 2nd year EPITA students
-** - website: http://huge.ocre.free.fr/
-** - svn repository: http://code.google.com/p/ocre
-**
-** About this folder: /extraction
-**   OCRec is the character extraction part of OCRe.
-** About this file: /extraction/args.h
-**   This is the header of "args.c".
-*/
+/**
+ * OCRe - The ultimate OCR - HUGE Software
+ * OCRe is a project developed by 2nd year EPITA students
+ * - website: http://huge.ocre.free.fr/
+ * - svn repository: http://code.google.com/p/ocre
+ *
+ * About this folder: /extraction
+ *   OCRec is the character extraction part of OCRe.
+ * About this file: /extraction/args.h
+ *   This is the header of "args.c".
+ */
+
 #ifndef ARGS_H
 #define ARGS_H
-
 #endif /* ARGS_H */
 
-/*
-** This function checks arguements and returns filenames of
-** input and output files and options which are activate.
-*/
+/**
+ * This function checks arguements and returns filenames of
+ * I/O files and options which are activate.
+ * @param argc Number of arguements.
+ * @param argv Array of strings passed in arguements.
+ */
 t_launch_infos *apply_args(int argc, char *argv[]);
