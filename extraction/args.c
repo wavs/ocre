54542be38c71f56daad957a20379eb00094fa34d
wavox.fr <wavox.fr@0426016d-d63e-0410-b645-ad462c66d616>
2008-03-23 22:09:22 +0000

    Correction args.c/h

--- extraction/args.c
+++ extraction/args.c
@@ -1,32 +1,35 @@
-/*
-** OCRe - The ultimate OCR - HUGE Software
-** OCRe is a project developed by 2nd year EPITA students
-** - website: http://huge.ocre.free.fr/
-** - svn repository: http://code.google.com/p/ocre
-**
-** About this folder: /extraction
-**   OCRec is the character extraction part of OCRe.
-** About this file: /extraction/args.c
-**   This file contains a function which checks the arguements.
-*/
+/**
+ * OCRe - The ultimate OCR - HUGE Software
+ * OCRe is a project developed by 2nd year EPITA students
+ * - website: http://huge.ocre.free.fr/
+ * - svn repository: http://code.google.com/p/ocre
+ *
+ * About this folder: /extraction
+ *   OCRec is the character extraction part of OCRe.
+ * About this file: /extraction/args.c
+ *   This file contains a function which checks the arguements.
+ */
 
 #include <stdlib.h>
+#include <string.h>
 #include "structures.h"
+#include "wrappers.h"
 #include "getopt.h"
 #include "infos.h"
-#include <string.h>
 
-/*
-** This function checks arguements and returns filenames of
-** input and output files and options which are activate.
-*/
+/**
+ * This function checks arguements and returns filenames of
+ * I/O files and options which are activate.
+ * @param argc Number of arguements.
+ * @param argv Array of strings passed in arguements.
+ */
 t_launch_infos *apply_args(int argc, char *argv[])
 {
   int optchar;
   t_launch_infos *res;
 
   res = NULL;
-  res = malloc(sizeof(t_launch_infos));
+  res = wmalloc(sizeof(t_launch_infos));
   res->display = 0;
   res->inFile = NULL;
   res->outFile = NULL;
@@ -41,7 +44,7 @@ t_launch_infos *apply_args(int argc, char *argv[])
 	      break;
 	    case 'h':
 	      help(argv[0]);
-	      free(res);
+	      wfree(res);
 	      exit(EXIT_SUCCESS);
 	    case 'd':
 	      res->display = 1;
@@ -49,12 +52,12 @@ t_launch_infos *apply_args(int argc, char *argv[])
 	    default:
 	    case '?':
 	      unknown_command_line(argv[0]);
-	      free(res);
+	      wfree(res);
 	      exit(EXIT_SUCCESS); }
 	}
     } else {
     help(argv[0]);
-    free(res);
+    wfree(res);
     exit(EXIT_SUCCESS);
     }
   return (res);
