21be39cc7768f5f4ca8f3a95122efe3c691dfdd4
wavox.fr <wavox.fr@0426016d-d63e-0410-b645-ad462c66d616>
2008-03-26 01:23:09 +0000

    Update fichiers segmentation FIXME | ne compile pas | pour support demain soir

---
+++ trunk/segmentation/main.c
@@ -0,0 +1,31 @@
+/**
+ * OCRe - The ultimate OCR - HUGE Software
+ * OCRe is a project developed by 2nd year EPITA students
+ * - website: http://huge.ocre.free.fr/
+ * - svn repository: http://code.google.com/p/ocre
+ *
+ * About this folder: /segmentation
+ *   OCRec is the character extraction part of OCRe.
+ * About this file: /segmentation/main.c
+ *   This is the main file. The program begins here.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include "structures.h"
+#include "projections.h"
+#include "wrappers.h"
+
+/**
+ * This is the main function.
+ *
+ * @param argc Number of arguements
+ * @param argv Array of string arguements
+ *
+ * @return Integer 0 if succeed, -1 if error
+ */
+int main()
+{
+  return (EXIT_SUCCESS);
+}
---
+++ trunk/segmentation/main.h
@@ -0,0 +1,26 @@
+/**
+ * OCRe - The ultimate OCR - HUGE Software
+ * OCRe is a project developed by 2nd year EPITA students
+ * - website: http://huge.ocre.free.fr/
+ * - svn repository: http://code.google.com/p/ocre
+ *
+ * About this folder: /segmentation
+ *   OCRec is the character extraction part of OCRe.
+ * About this file: /segmentation/main.h
+ *   This is the header of "main.c".
+ */
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#endif /* MAIN_H */
+
+/**
+ * This is the main function.
+ *
+ * @param argc Number of arguements
+ * @param argv Array of string arguements
+ *
+ * @return Integer 0 if succeed, -1 if error
+ */
+int main();
