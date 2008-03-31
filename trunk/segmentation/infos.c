03143e042c36699d9363b0666a1dffc7a5d856b4
wavox.fr <wavox.fr@0426016d-d63e-0410-b645-ad462c66d616>
2008-03-31 05:01:08 +0000

    Version qui marche avec des bmp 300 cest ca quest bon dixit axul | dsl double post

---
+++ trunk/segmentation/infos.c
@@ -0,0 +1,41 @@
+/**
+ * OCRe - The ultimate OCR - HUGE Software
+ * OCRe is a project developed by 2nd year EPITA students
+ * - website: http://huge.ocre.free.fr/
+ * - svn repository: http://code.google.com/p/ocre
+ *
+ * About this folder: /segmentation
+ *   OCRec is the character extraction part of OCRe.
+ * About this file: /segmentation/infos.c
+ *   This file contains some functions which show messages for user.
+ */
+
+#include <stdio.h>
+
+/**
+ * This function shows an error message when the user types
+ * a wrong command line.
+ * @param exe Name of the executable.
+ */
+void unknown_command_line(char *exe)
+{
+  printf("Unknown command line option...\n");
+  printf("Try: %s [-i input file] [-o output file]\n", exe);
+  printf("For more informations, type: %s -h\n", exe);
+}
+
+/**
+ * This function shows the short help of the program.
+ * @param exe Name of the executable.
+ */
+void help(char *exe)
+{
+ printf("Usage: %s -i inputfile -o output file\n\n", exe);
+ printf("This program extracts the connected ");
+ printf("components of a bitmap image\n");
+ printf("It saves the extraction result in an ouput bitmap file\n\n");
+ printf("Arguements:\n");
+ printf("   -i file: path of your input file\n");
+ printf("   -o file: path of your output file\n");
+ printf("   -h :     display the help\n\n");
+}
---
+++ trunk/segmentation/launch.c
@@ -0,0 +1,50 @@
+/**
+ * OCRe - The ultimate OCR - HUGE Software
+ * OCRe is a project developed by 2nd year EPITA students
+ * - website: http://huge.ocre.free.fr/
+ * - svn repository: http://code.google.com/p/ocre
+ *
+ * About this folder: /segmentation
+ *   OCRec is the character extraction part of OCRe.
+ * About this file: /segmentation/launch.c
+ *   This file contains functions which start the extraction
+ *   process.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "structures.h"
+#include "init.h"
+#include "process.h"
+#include "SDL/SDL.h"
+#include "infos.h"
+#include "tools.h"
+#include "wrappers.h"
+
+/**
+ * This function starts the extraction process without
+ * the display option.
+ * @param infos Informations about the launch process.
+ */
+void launchProgramWithoutDisplay(t_launch_infos *infos)
+{
+  SDL_Surface *screen;
+
+  screen = NULL;
+  if (infos->inFile != NULL && infos->outFile != NULL)
+    {
+      if (is_bmp(infos->inFile))
+	{
+	  InitSDL(screen);
+	  processAll(infos);
+	  SDL_Quit();
+	  printf("SDL closed.\n");
+	}
+      else
+	{
+	  help("extract");
+	  wfree(infos);
+	  exit(EXIT_SUCCESS);
+	}
+    }
+}
