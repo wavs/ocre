b5351b17eb45f9d39730d3adfabdf75caa3e21e9
guilbep <guilbep@0426016d-d63e-0410-b645-ad462c66d616>
2008-03-31 17:31:00 +0000

    rotation -left et -right fonctionnel

--- trunk/OCRed/rotation.ml
+++ trunk/OCRed/rotation.ml
@@ -264,6 +264,26 @@ let optimized3 tab angle =
     done;
   my_output
 
+let simple_left tab =
+  let height    = Bigarray.Array2.dim1 tab in
+  let width     = Bigarray.Array2.dim2 tab in
+  let my_output = Transforme.bigarray2 width height in
+    for i=(height -1) downto 0 do
+      for j=(width -1) downto 0 do
+        begin
+          Bigarray.Array2.set
+            my_output
+            (j)
+            (i)
+            (Bigarray.Array2.get
+               tab
+               ( height - i - 1)
+               ( j ));
+        end
+      done;
+    done;
+  my_output
+
 let simple_right tab =
   let height    = Bigarray.Array2.dim1 tab in
   let width     = Bigarray.Array2.dim2 tab in
@@ -278,27 +298,7 @@ let simple_right tab =
             (Bigarray.Array2.get
                tab
                ( i )
-               ( j ));
-        end
-      done;
-    done;
-  my_output
-
-let simple_left tab =
-  let height    = Bigarray.Array2.dim1 tab in
-  let width     = Bigarray.Array2.dim2 tab in
-  let my_output = Transforme.bigarray2 width height in
-    for i=0 to (height -1) do
-      for j=0 to (width -1) do
-        begin
-          Bigarray.Array2.set
-            my_output
-            (j)
-            (i)
-            (Bigarray.Array2.get
-               tab
-               ( i )
-               ( j ));
+               (width - 1 - j ));
         end
       done;
     done;
