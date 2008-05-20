7bcd12bfbaaa8fe0e132e8f7a7c3e88236595b71
guilbep <guilbep@0426016d-d63e-0410-b645-ad462c66d616>
2008-05-20 18:12:47 +0000

    jesus suce des ponney en enfer avec un god ceinture dans le cul

--- trunk/newnetwork/layer.mli
+++ trunk/newnetwork/layer.mli
@@ -4,4 +4,14 @@ class layer :
     val mutable bias : Neuron.neuron
     val mutable nbneurons : int
     val mutable neurons : Neuron.neuron array
+    method init_neurons : float -> unit
+    method set_bias : Neuron.neuron -> unit
+    method get_bias : unit -> Neuron.neuron
+
+    method get_tabneurons : unit -> Neuron.neuron array
+    method set_tabneurons : Neuron.neuron array -> unit
+
+    method set_nbneurons : int -> unit
+    method get_nbneurons : unit -> int
+
   end
--- trunk/newnetwork/neuron.mli
+++ trunk/newnetwork/neuron.mli
@@ -2,6 +2,12 @@ class neuron :
   object
     val mutable error : float
     val mutable nextweights : float array
-    val mutable predweight : float array
     val mutable value : float
+    method get_error : unit -> float
+    method get_nextweight : int -> float
+    method get_value : unit -> float
+    method init_nextweights : float -> unit
+    method set_error : float -> unit
+    method set_nextweight : int -> float -> unit
+    method set_value : float -> unit
   end
