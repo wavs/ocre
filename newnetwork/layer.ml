75cb253ad0848452a64dbe882adebac22ba3452d
guilbep <guilbep@0426016d-d63e-0410-b645-ad462c66d616>
2008-05-20 18:12:47 +0000

    jesus suce des ponney en enfer avec un god ceinture dans le cul

--- newnetwork/layer.ml
+++ newnetwork/layer.ml
@@ -4,5 +4,16 @@ object(self)
   val mutable nbneurons = nbn
   val mutable neurons = Array.make nbn (new Neuron.neuron)
   val mutable bias = new Neuron.neuron
+  method init_neurons nb_arc_avt =
+    for i = 0 to Array.length  neurons - 1 do
+      (neurons.(i))#init_nextweights nb_arc_avt
+    done;
+  method set_nbneurons x  = nbneurons <- x
+  method get_nbneurons () = nbneurons
 
+  method set_bias x  = bias <- x
+  method get_bias () = bias
+
+  method get_tabneurons () = neurons
+  method set_tabneurons x = neurons <- x
 end
--- newnetwork/neuron.ml
+++ newnetwork/neuron.ml
@@ -4,9 +4,24 @@ object(self)
   val mutable value = 0.
   val mutable error = 0.
   val mutable nextweights = Array.make 1 1.
-  val mutable predweight = Array.make 1 1.
+  method get_error () = error
+  method set_error x = error <- x
 
-  method get_value = value
-  method set_value x = value <- x 
+  method get_value () = value
+  method set_value x = value <- x
+
+  method init_nextweights taille =
+    let tab = Array.make 1 taille in
+    for i = 0 to Array.length tab - 1 do
+      let alea = Random.float 1. in
+        if alea > 0.5 then
+          Array.set tab i (Random.float 10.)
+        else
+          Array.set tab i (Random.float (-. 10.))
+    done;
+   nextweights <- tab
+
+  method set_nextweight pos value = Array.set nextweights pos value
+  method get_nextweight pos = Array.get nextweights pos
 
 end
