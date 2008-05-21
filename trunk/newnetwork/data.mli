720325fa6694f2bc450bf6bdb915e2be16528cd0
guilbep <guilbep@0426016d-d63e-0410-b645-ad462c66d616>
2008-05-21 02:56:10 +0000

    entrain de modifier le testml pour pouvoir voir si on initialise vraiment bien avec un pattern

--- trunk/newnetwork/data.mli
+++ trunk/newnetwork/data.mli
@@ -6,4 +6,16 @@ class data :
     val mutable nbinputs : int
     val mutable nboutputs : int
     val mutable outputs : int array
+    method get_input : int -> int
+    method get_inputs : unit -> int array
+    method get_nbinputs : int
+    method get_nboutputs : unit -> int
+    method get_output : int -> int
+    method get_outputs : unit -> int array
+    method set_input : int -> int -> unit
+    method set_inputs : int array -> unit
+    method set_nbinputs : int -> unit
+    method set_nboutputs : int -> unit
+    method set_output : int -> int -> unit
+    method set_outputs : int array -> unit
   end
