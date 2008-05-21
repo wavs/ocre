01447df573e45b0f4a3cd1a7d34ab9c69d8a6247
atestu <atestu@0426016d-d63e-0410-b645-ad462c66d616>
2008-05-21 20:47:17 +0000

    a plus de dossier pornhub, ça aurait fait tout drole dans la tarball après un svn export...

--- trunk/pornhub/pornhub.ml
+++
@@ -1,9 +0,0 @@
-let main() = 
-  for i = 0 to 42000 do
-    print_string "HOT! www.pOrnhub.com HOT!";
-    print_newline();
-    print_string "It makes your dick HUGER !!!!";
-    print_newline()
-  done
-
-let _ = main()
