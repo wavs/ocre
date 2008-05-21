4a9f634cd8b4c274d991fe9df14005d2910cce39
guilbep <guilbep@0426016d-d63e-0410-b645-ad462c66d616>
2008-05-21 22:14:53 +0000

    mise a jour des erreurs

--- newnetwork/perceptron.mli
+++ newnetwork/perceptron.mli
@@ -9,17 +9,18 @@ object
   val mutable nblayers : int
   val mutable patterns : Data.tab_xor
   val mutable quad_error : float
+  method backpropagation : int -> unit
   method get_nblayers : unit -> int
   method get_quad : unit -> float
-  method set_quad : float -> unit
   method init : unit -> unit
   method learn : unit -> unit
   method print_layer : unit -> unit
   method print_layer_and_weight : unit -> unit
+  method refresh_hidden_neurons_value : int -> unit
+  method set_err_quad : int -> unit
+  method set_error_for_ouput_neurons : int -> unit
   method set_forward_propagate : unit -> unit
   method set_input_pattern : int -> unit
   method set_nblayers : int -> unit
-  method set_error_for_ouput_neurons : int -> unit
-  method set_err_quad : int -> unit
-  method backpropagation : int -> unit
+  method set_quad : float -> unit
 end
